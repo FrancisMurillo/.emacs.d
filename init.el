e6db8dcaf27f364deeac3b48ba4efe28d82661f6
FrancisMurillo <francisavmurillo@gmail.com>
2016-01-10 10:51:27 +0800

    Updating readme

--- init.el
+++ init.el
@@ -2,20 +2,17 @@
 (package-initialize nil)
 
 ;; Override the packages with the git version of Org and other packages
+
+;; Static org-mode
 (add-to-list 'load-path 
-	     (expand-file-name "elisp/org-mode/lisp" user-emacs-directory))
+	     (expand-file-name "elisp/org-mode/lisp" user-emacs-directory)) 
 (add-to-list 'load-path 
 	     (expand-file-name "elisp/org-mode/contrib/lisp" user-emacs-directory))
-
-
-;; (add-to-list 'load-path "~/code/org2blog")
-;; (add-to-list 'load-path "~/Dropbox/2014/presentations/org-reveal")
-;; (add-to-list 'load-path "/usr/local/share/emacs/site-lisp")
 
 ;; Load the rest of the packages
 (package-initialize nil)
 
-;; Initialize manually?
+;; FIXME: Can loading org not be hard coded?
 (load-library "org")
 
 (setq package-enable-at-startup nil)
