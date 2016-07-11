0e974eef97acfff0258ec141a4fadd5f3d2baccb
Francis Murillo <francisavmurillo@gmail.com>
2016-07-11 16:05:49 +0800

    Updating with stuff

--- init.el
+++ init.el
@@ -1,9 +1,9 @@
-;; This customized init file allows to load a default home or a custom home to
+;; This customized init file allows to load a default home or a custom
+;; home to
 
 (setq fn/home-switch "-H")
 
-(setq fn/standard-init-file "init-standard.el"
-      fn/default-init-file "init.el")
+(setq fn/standard-init-file "init-standard.el" fn/default-init-file "init.el")
 
 
 (defun fn/get-switch-arg (switch)
@@ -13,12 +13,12 @@
       nil)))
 
 (defun fn/bootstrap-emacs ()
-  ;;; Reference: https://stackoverflow.com/questions/2112256/emacs-custom-command-line-argument
+;;; Reference: https://stackoverflow.com/questions/2112256/emacs-custom-command-line-argument
   (let ((new-home (fn/get-switch-arg fn/home-switch))
         (bootstrap-file (expand-file-name fn/standard-init-file user-emacs-directory)))
     (when new-home
-      (setq user-emacs-directory new-home
-            user-init-file (expand-file-name fn/default-init-file user-emacs-directory)
+      (setq user-emacs-directory new-home user-init-file (expand-file-name fn/default-init-file
+                                                                           user-emacs-directory)
             bootstrap-file user-init-file))
     (load bootstrap-file))
 
@@ -26,16 +26,9 @@
   (add-to-list 'command-switch-alist '(fn/home-switch . identity)))
 
 (defun fn/bootstrap-new-emacs (new-home)
-  (async-shell-command (mapconcat 'identity
-                                  (list "emacs"
-                                        "--debug-init"
-                                        fn/home-switch
-                                        new-home)
+  (async-shell-command (mapconcat 'identity (list "emacs" "--debug-init" fn/home-switch new-home)
                                   " "))
-  (message (mapconcat 'identity (list
-                                 "Bootstrapped Emacs from"
-                                 new-home
-                                 "so go open that window since I can't... for now")
-                      " ")))
+  (message (mapconcat 'identity (list "Bootstrapped Emacs from" new-home
+                                      "so go open that window since I can't... for now") " ")))
 
 (fn/bootstrap-emacs)
