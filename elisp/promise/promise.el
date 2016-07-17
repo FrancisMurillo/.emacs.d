e8218b7a7728b8a17a3d907bad97593ac9f5d172
Francis Murillo <francisavmurillo@gmail.com>
2016-07-17 22:17:28 +0800

    Update

--- elisp/promise/promise.el
+++ elisp/promise/promise.el
@@ -17,34 +17,41 @@
 (defun p-make-promise (executor)
   "Create a promise that is handled by an executor."
   (lexical-let* ((promised nil)
-                 (run-hooks (lambda ()
-                              (pcase-let ((`(,state ,promised-value ,fulfilled-hooks ,rejected-hooks) promised))
-                                (pcase state
-                                  (`pending nil)
-                                  (`fulfilled (mapc (lambda (hook) (funcall hook promised-value))
-                                                    fulfilled-hooks))
-                                  (`rejected (mapc (lambda (hook) (funcall hook promised-value))
-                                                   rejected-hooks))))))
+                 (run-hooks
+                  (lambda ()
+                    (pcase-let ((`(,state ,promised-value ,fulfilled-hooks ,rejected-hooks) promised))
+                      (pcase state
+                        (`pending nil)
+                        (`fulfilled (mapc (lambda (hook) (funcall hook promised-value))
+                                          fulfilled-hooks))
+                        (`rejected (mapc (lambda (hook) (funcall hook promised-value))
+                                         rejected-hooks))))))
                  (in-context t)
-                 (fulfiller (lambda (value)
-                              (pcase-let ((`(,state ,promised-value ,_ ,_) promised))
-                                (when (eq state 'pending)
-                                  (setf (nth 0 promised) 'fulfilled)
-                                  (setf (nth 1 promised) value)
-                                  (unless in-context
-                                    (funcall run-hooks))))))
-                 (rejector (lambda (reason)
-                             (pcase-let ((`(,state ,promised-value ,_ ,_) promised))
-                               (when (eq state 'pending)
-                                 (setf (nth 0 promised) 'rejected)
-                                 (setf (nth 1 promised) reason)
-                                 (unless in-context
-                                   (funcall run-hooks)))))))
+                 (fulfiller
+                  (lambda (value)
+                    (pcase-let ((`(,state ,promised-value ,_ ,_) promised))
+                      (when (eq state 'pending)
+                        (setf (nth 0 promised) 'fulfilled)
+                        (setf (nth 1 promised) value)
+                        (unless in-context
+                          (funcall run-hooks))))))
+                 (rejector
+                  (lambda (reason)
+                    (pcase-let ((`(,state ,promised-value ,_ ,_) promised))
+                      (when (eq state 'pending)
+                        (setf (nth 0 promised) 'rejected)
+                        (setf (nth 1 promised) reason)
+                        (unless in-context
+                          (funcall run-hooks)))))))
     (setq promised (p--promise 'pending nil (list) (list)))
     (funcall executor fulfiller rejector)
     (setq in-context nil)
     (funcall run-hooks)
     promised))
+
+(defun p-state (promise)
+  "Get the state of a promise, not to be used externally. Just for testing"
+  (car promise))
 
 (defun p-promise-p (value)
   "Check if value is a promise"
@@ -61,7 +68,7 @@
     (pcase state
       (`fulfilled (when (functionp fulfilled)
                     (funcall fulfilled promise-value)))
-      (`rejected (when (funcionp rejected)
+      (`rejected (when (functionp rejected)
                    (funcall rejected promise-value)))
       (`pending
        (when (functionp fulfilled)
@@ -71,55 +78,45 @@
   promise)
 
 
-(defun p-all (promise &rest other-promises)
+(defun p-all (&rest promises)
   "Creates a promise that is fulfilled when all promises are fulfilled
    and rejected when any of it is rejected"
-  (lexical-let ((promises (append (list promise) other-promises)))
-    (p-make-promise
-     (lambda (fulfiller rejector)
-       (lexical-let* ((fulfiller fulfiller)
-                      (rejector rejector)
-                      (fulfilled-promises 0)
-                      (values (-repeat (length promises) nil ))
-                      (all-fulfilled
-                       (lambda (value index)
-                         (setq fulfilled-promises (1+ fulfilled-promises)
-                               values (-replace-at index value values))
-                         (when (= fulfilled-promises (length promises))
-                           (funcall fulfiller values))))
-                      (any-rejected
-                       (lambda (reason)
-                         (funcall rejector reason))))
-         (-map-indexed (lambda (index promise)
-                         (lexical-let ((index index))
-                           (p-then promise
-                                   (lambda (value)
-                                     (funcall all-fulfilled value index))
-                                   (lambda (reason)
-                                     (funcall any-rejected reason)))))
-                       promises))))))
+  (p-make-promise
+   (lambda (fulfiller rejector)
+     (lexical-let* ((fulfiller fulfiller)
+                    (rejector rejector)
+                    (fulfilled-promises 0)
+                    (values (-repeat (length promises) nil ))
+                    (all-fulfilled
+                     (lambda (value index)
+                       (setq fulfilled-promises (1+ fulfilled-promises)
+                             values (-replace-at index value values))
+                       (when (= fulfilled-promises (length promises))
+                         (funcall fulfiller values))))
+                    (any-rejected
+                     (lambda (reason)
+                       (funcall rejector reason))))
+       (-map-indexed (lambda (index promise)
+                       (lexical-let ((index index))
+                         (p-then promise
+                                 (lambda (value)
+                                   (funcall all-fulfilled value index))
+                                 (lambda (reason)
+                                   (funcall any-rejected reason)))))
+                     promises)))))
 
-(defun p-all-features (feature &rest other-features)
+
+(defun p-all-features (&rest features)
   "Creates a promise that is fulfilled when all features are loaded.
-
    Useful when loading packages with dependencies and the reason this library is done"
-  (lexical-let ((features (append (list feature) other-features)))
-    (apply #'p-all (mapcar
-                    (lambda (feature)
-                      (p-make-promise
-                       (lambda (fulfiller _)
-                         (eval-after-load feature
-                           (progn
-                             (funcall fulfiller feature))))))
-                    features))))
-
-(defmacro p-on-features (features &rest body )
-  "A short macro to ease the use of using p-all-features"
-  `(p-then
-    (p-all-features ,@features)
-    (lambda (loaded-features)
-      ,@body
-      )))
+  (apply #'p-all (mapcar
+                  (lambda (feature)
+                    (p-make-promise
+                     (lambda (fulfiller _)
+                       (eval-after-load feature
+                         (when (featurep feature)
+                           (funcall fulfiller feature))))))
+                  features)))
 
 
 (provide 'promise)
