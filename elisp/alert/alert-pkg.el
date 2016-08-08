619d5959f83504dd5fc504736abfe913f0d85b0d
Francis Murillo <francisavmurillo@gmail.com>
2016-08-08 09:02:10 +0800

    Update with custom alert

---
+++ elisp/alert/alert-pkg.el
@@ -0,0 +1,1 @@
+(define-package "alert" "20160506.1821" "Growl-style notification system for Emacs" '((gntp "0.1") (log4e "0.3.0")) :stars '(#("102" 0 3 (font-lock-face paradox-star-face))) :url "https://github.com/jwiegley/alert" :keywords '("notification" "emacs" "message"))
