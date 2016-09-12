ce514bb0802484ea051700be78316006c3028eb4
Francis Murillo <francisavmurillo@gmail.com>
2016-09-12 18:43:02 +0800

    Updating promise library

--- elisp/promise/promise.el
+++ elisp/promise/promise.el
@@ -1,87 +1,162 @@
-;;; package --- Summary
-
+;;; promise.el --- A simple promise library for my Emacs configuration  -*- lexical-binding: t; -*-
+;;
+;; Filename: promise.el
+;; Description: A simple promise library for my Emacs configuration
+;; Author: Francis Murillo
+;; Maintainer: Francis Murillo
+;; Created: Sat Aug 27 18:59:25 2016 (+0800)
+;; Version: 0.10
+;; Package-Requires: ()
+;; Last-Updated:
+;;           By:
+;;     Update #: 0
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
 ;;; Commentary:
-
-;;; Packages
-
-;; -*- lexical-binding: t; -*-
-
-(require 'dash)
-
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
 ;;; Code:
 
-(defun p--promise (state &optional value fulfilled-hooks rejected-hooks)
-  "Create a promise type."
+(require 'cl-lib)
+
+
+;;* Private
+(defun promise--make (state &optional value fulfilled-hooks rejected-hooks)
+  "Create a promise with STATE, VALUE, FULFILLED-HOOKS and REJECTED-HOOKS."
   (list state value fulfilled-hooks rejected-hooks))
 
-(defun p-make-promise (executor)
-  "Create a promise that is handled by an executor."
-  (lexical-let* ((promised nil)
-                 (run-hooks
-                  (lambda ()
-                    (pcase-let ((`(,state ,promised-value ,fulfilled-hooks ,rejected-hooks) promised))
-                      (pcase state
-                        (`pending nil)
-                        (`fulfilled (mapc (lambda (hook) (funcall hook promised-value))
-                                          fulfilled-hooks))
-                        (`rejected (mapc (lambda (hook) (funcall hook promised-value))
-                                         rejected-hooks))))))
-                 (in-context t)
-                 (fulfiller
-                  (lambda (value)
-                    (pcase-let ((`(,state ,promised-value ,_ ,_) promised))
-                      (when (eq state 'pending)
-                        (setf (nth 0 promised) 'fulfilled)
-                        (setf (nth 1 promised) value)
-                        (unless in-context
-                          (funcall run-hooks))))))
-                 (rejector
-                  (lambda (reason)
-                    (pcase-let ((`(,state ,promised-value ,_ ,_) promised))
-                      (when (eq state 'pending)
-                        (setf (nth 0 promised) 'rejected)
-                        (setf (nth 1 promised) reason)
-                        (unless in-context
-                          (funcall run-hooks)))))))
-    (setq promised (p--promise 'pending nil (list) (list)))
+(defun promise--state (promise)
+  "Get the state of a PROMISE, not to be used externally.  Mainly for unit testing."
+  (car promise))
+
+
+;;* Interface
+(defun promise (executor)
+  "Create a promise that is handled by an EXECUTOR."
+  (letrec
+      ((promised nil)
+       (in-context t)
+       (fulfiller
+        (lambda (value)
+          (pcase-let ((`(,state ,promised-value ,_ ,_) promised))
+            (when (eq state 'pending)
+              (setf (nth 0 promised) 'fulfilled)
+              (setf (nth 1 promised) value)
+              (unless in-context
+                (funcall run-hooks))))))
+       (rejector
+        (lambda (reason)
+          (pcase-let ((`(,state ,promised-value ,_ ,_) promised))
+            (when (eq state 'pending)
+              (setf (nth 0 promised) 'rejected)
+              (setf (nth 1 promised) reason)
+              (unless in-context
+                (funcall run-hooks))))))
+       (run-hooks
+        (lambda ()
+          (pcase-let ((`(,state ,promised-value ,fulfilled-hooks ,rejected-hooks) promised))
+            (if (eq state 'pending)
+                nil
+              (if (promise-p promised-value)
+                  (promise-then
+                   promised-value
+                   (lambda (value)
+                     (funcall fulfiller value))
+                   (lambda (value)
+                     (funcall rejector value))))
+              (pcase state
+                (`fulfilled (mapc (lambda (hook) (funcall hook promised-value))
+                                  fulfilled-hooks))
+                (`rejected (mapc (lambda (hook) (funcall hook promised-value))
+                                 rejected-hooks))))))))
+    (setq promised (promise--make 'pending nil (list) (list)))
     (funcall executor fulfiller rejector)
     (setq in-context nil)
     (funcall run-hooks)
     promised))
 
-(defun p-state (promise)
-  "Get the state of a promise, not to be used externally. Just for testing"
-  (car promise))
-
-(defun p-promise-p (value)
-  "Check if value is a promise"
+(defun promise-p (value)
+  "Check if VALUE is a promise."
   (pcase value
-    (`(`pending `nil ,_ ,_) t)
-    (`(`fulfilled ,_ ,_ ,_) t)
-    (`(`rejected ,_ ,_ ,_) t)
+    (`(pending ,_ ,_ ,_) t)
+    (`(fulfilled ,_ ,_ ,_) t)
+    (`(rejected ,_ ,_ ,_) t)
     (_ nil)))
 
+(defun promise-then (promise &optional fulfilled rejected)
+  "Add a hook into a PROMISE with FULFILLED and REJECTED resolution."
+  (promise
+   (lambda (res rej)
+     (lexical-let
+         ((wrapped-resolved
+           (lambda (value)
+             (let ((new-value (funcall fulfilled value)))
+               (if (promise-p new-value)
+                   (promise-then
+                    new-value
+                    (lambda (newer-value)
+                      (funcall res newer-value))
+                    (lambda (newer-value)
+                      (funcall rej newer-value)))
+                 (funcall res new-value)))))
+          (wrapped-rejected
+           (lambda (value)
+             (let ((new-value (funcall rejected value)))
+               (if (promise-p new-value)
+                   (promise-then
+                    new-value
+                    (lambda (newer-value)
+                      (funcall res newer-value))
+                    (lambda (newer-value)
+                      (funcall rej newer-value)))
+                 (funcall rej new-value))))))
+       (pcase-let
+           ((`(,state ,promise-value ,fulfilled-hooks ,rejected-hooks) promise))
+         (pcase state
+           (`fulfilled
+            (when (functionp fulfilled)
+              (funcall wrapped-resolved promise-value)))
+           (`rejected
+            (when (functionp rejected)
+              (funcall wrapped-rejected promise-value)))
+           (`pending
+            (when (functionp fulfilled)
+              (push wrapped-resolved (nth 2 promise)))
+            (when (functionp rejected)
+              (push wrapped-rejected (nth 3 promise))))))))))
 
-(defun p-then (promise &optional fulfilled rejected)
-  "Add a hook into a promise resolution"
-  (pcase-let ((`(,state ,promise-value ,fulfilled-hooks ,rejected-hooks) promise))
-    (pcase state
-      (`fulfilled (when (functionp fulfilled)
-                    (funcall fulfilled promise-value)))
-      (`rejected (when (functionp rejected)
-                   (funcall rejected promise-value)))
-      (`pending
-       (when (functionp fulfilled)
-         (push fulfilled (nth 2 promise)))
-       (when (functionp rejected)
-         (push rejected (nth 3 promise))))))
-  promise)
 
-
-(defun p-all (&rest promises)
-  "Creates a promise that is fulfilled when all promises are fulfilled
-   and rejected when any of it is rejected"
-  (p-make-promise
+(defun promise-all (&rest promises)
+  "Create a promise that is fulfilled when all PROMISES are fulfilled and rejected when any of it is rejected."
+  (promise
    (lambda (fulfiller rejector)
      (lexical-let* ((fulfiller fulfiller)
                     (rejector rejector)
@@ -98,26 +173,43 @@
                        (funcall rejector reason))))
        (-map-indexed (lambda (index promise)
                        (lexical-let ((index index))
-                         (p-then promise
-                                 (lambda (value)
-                                   (funcall all-fulfilled value index))
-                                 (lambda (reason)
-                                   (funcall any-rejected reason)))))
+                         (promise-then promise
+                                       (lambda (value)
+                                         (funcall all-fulfilled value index))
+                                       (lambda (reason)
+                                         (funcall any-rejected reason)))))
                      promises)))))
 
+(defun promise-idle-delay (delay promise)
+  "Wrap a promise with a delay so that it would not block the interface
+ by DELAY and PROMISE."
+  (promise-then
+   promise
+   (lambda (value)
+     (promise
+      (lambda (res _)
+        (run-with-idle-timer
+         delay
+         nil
+         (lambda ()
+           (funcall res value))))))))
 
-(defun p-all-features (&rest features)
-  "Creates a promise that is fulfilled when all features are loaded.
-   Useful when loading packages with dependencies and the reason this library is done"
-  (apply #'p-all (mapcar
-                  (lambda (feature)
-                    (p-make-promise
-                     (lambda (fulfiller _)
-                       (eval-after-load feature
-                         (when (featurep feature)
-                           (funcall fulfiller feature))))))
-                  features)))
+
+(defun promise-all-features (&rest features)
+  "Create a promise that is fulfilled when all FEATURES are loaded.  Useful when loading packages with dependencies and the reason this library is done."
+  (apply
+   #'promise-all
+   (mapcar
+    (lambda (feature)
+      (promise
+       (lambda (fulfiller _)
+         (eval-after-load feature
+           (when (featurep feature)
+             (funcall fulfiller feature))))))
+    features)))
 
 
 (provide 'promise)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; promise.el ends here
--- init.el
+++ init.el
@@ -1,6 +1,13 @@
 ;; This customized init file allows to load a default home or a custom
 ;; home to
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;; (package-initialize) ;; This is handled by `init-standard.el` instead.
+
 (setq fn/home-switch "-H")
 
 (setq fn/standard-init-file "init-standard.el" fn/default-init-file "init.el")
@@ -32,3 +39,24 @@
                                       "so go open that window since I can't... for now") " ")))
 
 (fn/bootstrap-emacs)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (helm-smex smex helm-fuzzier helm-flx jekyll-modes rainbow-mode contrast-color colorsarenice-theme ocodo-svg-modelines zone-sl zone-rainbow zone-nyan workgroups2 window-numbering which-key wgrep-helm web-mode web-beautify virtualenvwrapper virtualenv use-package undo-tree twittering-mode tronesque-theme telephone-line spaceline smartparens smart-mode-line-powerline-theme slack skewer-mode shm screenshot sass-mode rvm robe react-snippets rainbow-delimiters py-autopep8 prodigy polymode pcomplete-extension pcmpl-pip pcmpl-git paredit-everywhere paradox pacmacs ox-reveal overseer org-plus-contrib org-journal omnisharp octicons nyan-mode npm-mode nodejs-repl micgoline memory-usage main-line magit-svn lispy ledger-mode latex-preview-pane keyfreq json-snatcher jsfmt js2-refactor js-doc jedi jdee jabber hungry-delete hindent helm-swoop helm-projectile helm-describe-modes helm-descbinds helm-ag header2 haskell-snippets haskell-emacs golden-ratio github-clone git-timemachine git-gutter gist ggtags free-keys flycheck-pos-tip flycheck-mix flycheck-ledger flycheck-haskell flycheck-elm flycheck-elixir flappymacs fireplace fill-column-indicator figlet f3 expand-region eslint-fix enh-ruby-mode engine-mode emr emms emmet-mode emamux emacsshot elpy elm-yasnippets elm-mode elixir-yasnippets elfeed elein elang ein dumb-jump dockerfile-mode docker counsel conkeror-minor-mode company-web company-tern company-jedi company-ghci company-ghc company-auctex command-log-mode column-marker color-theme-approximate color-identifiers-mode cask-mode cask buttercup bbdb base16-theme auto-compile aurel apropospriate-theme alchemist airline-themes ag))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "dark orange"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "deep pink"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "chartreuse"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "deep sky blue"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "yellow"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "orchid"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "spring green"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "sienna1")))))
