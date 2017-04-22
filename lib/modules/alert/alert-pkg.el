669fb317719de65d368bad3894c8b40164604373
Francis Murillo <francisavmurillo@gmail.com>
2017-04-22 23:49:12 +0800

    Reintegrate upstream =alert= instead of custom

--- lib/modules/alert/alert-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "alert" "20160506.1821" "Growl-style notification system for Emacs" '((gntp "0.1") (log4e "0.3.0")) :stars '(#("102" 0 3 (font-lock-face paradox-star-face))) :url "https://github.com/jwiegley/alert" :keywords '("notification" "emacs" "message"))
