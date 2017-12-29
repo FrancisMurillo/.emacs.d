2d4407c2713455e433e0cfdb18486b3b65ed5215
Francis Murillo <francis@invento.io>
2017-12-30 02:50:11 +0800

    Refine tronesque to separate package

---
+++ lib/modules/tronesque/tronesque-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; tronesque-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "tronesque-theme" "tronesque-theme.el" (22653
+;;;;;;  14466 248001 490000))
+;;; Generated autoloads from tronesque-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; tronesque-theme-autoloads.el ends here
---
+++ lib/modules/tronesque/tronesque-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "tronesque-theme" "20150125.241" "Color Theme based on Tron universe." 'nil :url "https://github.com/aurelienbottazini/tronesque")
