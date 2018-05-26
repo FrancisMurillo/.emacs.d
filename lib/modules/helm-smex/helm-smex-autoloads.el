bea62232b79dbb60dbfdbcbb25b1316a0c9dbb9c
Francis Murillo <francisavmurillo@gmail.com>
2018-05-26 20:31:59 +0800

    Refactor smex and helm-smex to archive

---
+++ lib/modules/helm-smex/helm-smex-autoloads.el
@@ -0,0 +1,27 @@
+;;; helm-smex-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "helm-smex" "helm-smex.el" (22653 12781 878048
+;;;;;;  209000))
+;;; Generated autoloads from helm-smex.el
+
+(autoload 'helm-smex "helm-smex" "\
+
+
+\(fn)" t nil)
+
+(autoload 'helm-smex-major-mode-commands "helm-smex" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; helm-smex-autoloads.el ends here
